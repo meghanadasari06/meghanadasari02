72cf3dca25ab84a81d87336e4e5d27511a73e3d3
meghanadasari06 <88731223+meghanadasari06@users.noreply.github.com>
2021-08-13 14:08:09 +0530

    Add files via upload

---
+++ program2.py
@@ -0,0 +1,51 @@
+# aritametic operator 
+
+x,y,z=2,6,10
+
+print("\n 2+6=",x+y)
+print("\n 10-6=",z-y)
+print("\n 2*6=",x*y)
+print("\n 10/2=",x/y)
+
+
+# comparision operator
+
+print("\n x<y",x<y)
+print("\n x>y",x>y)
+print("\n x==y",x==y);print("\n x!=y",x!=y)
+
+# logical operator 
+
+m=False
+n=True
+
+print("\n m and n is",m and n)
+print("\n a and n is",m and n)
+print("\n not n is",not n)
+
+# identity operator 
+
+x1=10
+x2="hlo"
+x3=[1,2,3]
+y1=10
+y2="hlo"
+y3=[1,2,3]
+
+print("\n x1 is not y2",x1 is not y2)
+print("\n x1 is not y1",x1 is not y1)
+print("\n x2 is y2",x2 is y3)
+print("\n x3 is y2",x3 is y2)
+
+# membership operator 
+
+m1="hlo"
+m2={1:'a',2:'b'}
+
+print('h' in m1)
+print('hlo' not in m1)
+print(1 in m2)
+print('a' in m2)
+
+
+
