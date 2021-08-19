539ae35828e8c877265827e7a7bcad3c712bb314
meghanadasari06 <88731223+meghanadasari06@users.noreply.github.com>
2021-08-19 13:12:49 +0530

    Add files via upload

---
+++ Assign_Q1 (1).py
@@ -0,0 +1,5 @@
+# Reverse a String and print it on console "Python Skills"
+str = "Python Skills"
+print("Before reversing string: " + str)
+str = str[::-1]                            
+print("After reversing string: " + str)
