cb35d3ddc820fa85306ce34ab9a80a7c9f79b46f
meghanadasari06 <88731223+meghanadasari06@users.noreply.github.com>
2021-08-28 20:51:54 +0530

    Add files via upload

---
+++ Assignment2 #1.py
@@ -0,0 +1,18 @@
+f=open("math.txt",'r',encoding='utf-8')
+x=f.readlines()
+y=x[0].split()
+
+add=0
+sub=0
+mul=0
+
+
+for i in y:
+   
+    add = add + int(i)
+    sub = sub - int(i)
+    mul = mul * int(i)
+
+print("\nAddition is: ",add)
+print("\nSubtraction is: ",sub)  
+print("\nMultiplication is: ",mul) 
